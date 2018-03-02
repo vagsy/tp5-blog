0d743195d9ebd68ee910a08d43e0611afaa8a9b5
vagsy <981604339@qq.com>
2018-03-02 17:49:14 +0800

    fix

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -56,31 +56,43 @@ class Article extends Controller
     }
 
     public function edit(){
-    	$id=input('id');
-    	$article=db('article')->find($id);
-    	if(request()->isPost()){
-    		$data=[
-    			'id'=>input('id'),
-    			'title'=>input('title'),
-                'url'=>input('url'),
+        $id=input('id');
+        $articles=db('Article')->find($id);
+        if(request()->isPost()){
+            $data=[
+                'id'=>input('id'),
+                'title'=>input('title'),
+                'author'=>input('author'),
                 'desc'=>input('desc'),
-    		];
-
-    		$validate = \think\Loader::validate('Article');
-    		if (!$validate->scene('edit')->check($data)) {
-    			$this->error($validate->getError());
-    			die;
-    		}
-
-    		if(db('article')->update($data)){
-    			$this->success('修改文章信息成功！', 'lst');
-    		}else{
-    			$this->error('修改文章失败！');
-    		}
-    		return;
-    	}
-    	// dump($admins);die;
-    	$this->assign('article', $article);
+                'keywords'=>str_replace('，', ',', input('keywords')),
+                'content'=>input('content'),
+                'cateid'=>input('cateid'),
+            ];
+            if(input('state')=='on'){
+                $data['state']=1;
+            }else{
+                $data['state']=0;
+            }
+            if($_FILES['pic']['tmp_name']){
+                
+                $file = request()->file('pic');
+                $info = $file->move(ROOT_PATH . 'public' . DS . 'static/uploads');
+                $data['pic']='/uploads/'.$info->getSaveName();
+            }
+            $validate = \think\Loader::validate('Article');
+            if(!$validate->scene('edit')->check($data)){
+               $this->error($validate->getError()); die;
+            }
+            if(db('Article')->update($data)){
+                $this->success('修改文章成功！','lst');
+            }else{
+                $this->error('修改文章失败！');
+            }
+            return;
+        }
+        $this->assign('articles',$articles);
+        $cateres=db('cate')->select();
+        $this->assign('cateres',$cateres);
         return $this->fetch();
     }
 
